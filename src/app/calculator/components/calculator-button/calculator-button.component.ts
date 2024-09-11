bf9c376464701768355f527e9d7fa5b0c42256e4
Leonel Salcedo <nomackleo@gmail.com>
2024-09-10 19:25:25 -0500

    Basic operation of the calculator, manage of Host and signals.

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,18 +1,88 @@
-import { Component } from '@angular/core';
+import { CalculatorComponent } from '@/calculator/components/calculator/calculator.component';
+import {
+  asNativeElements,
+  Component,
+  ElementRef,
+  HostBinding,
+  input,
+  output,
+  signal,
+  viewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
   standalone: true,
   imports: [],
   template: `
-    <div class="w-1/4 border-r border-b border-indigo-400">
-      <button
-        class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-opacity-50 text-xl font-light"
-      >
-        <ng-content #nameCalculatorButton></ng-content>
-      </button>
-    </div>
+    <button
+      [class.is-command]="isCommand()"
+      [class.is-pressed]="isPressed()"
+      (click)="handleClick()"
+      #calculatorButton
+    >
+      <ng-content #nameCalculatorButton></ng-content>
+    </button>
   `,
-  styles: ``,
+  styles: `
+  button {
+    @apply w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light;
+  }
+  .is-command {
+    @apply bg-indigo-700 bg-opacity-10;
+  }
+  .is-pressed {
+    @apply bg-indigo-800 bg-opacity-50;
+  }
+  `,
+  host: {
+    class: 'border-r border-b border-indigo-400',
+    '[class.w-2/4]': 'isDoubleSize()',
+    '[class.w-1/4]': '!isDoubleSize()',
+  },
 })
-export class CalculatorButtonComponent {}
+export class CalculatorButtonComponent {
+  public onClick = output<string>();
+  public contentValue =
+    viewChild<ElementRef<HTMLButtonElement>>('calculatorButton');
+
+  public isPressed = signal(false);
+
+  public isCommand = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value,
+  });
+  public isDoubleSize = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value,
+  });
+
+  /**
+   * Styles for button command, in this case, it will apply directly in the button.
+   * Styles are implmented via hte isCommand() signal directly in the button.
+   * Verify how was implemented in styles of this component: {@link CalculatorComponent}
+   */
+  // @HostBinding('class.w-2/4') get commandStyle() {
+  //   return this.isDoubleSize();
+  // }
+
+  handleClick() {
+    if (!this.contentValue()!.nativeElement) return;
+    const value = this.contentValue()!.nativeElement.innerText;
+
+    this.onClick.emit(value.trim());
+  }
+
+  keyboardPressedStyle(key: string) {
+    if (!this.contentValue()) return;
+    const value = this.contentValue()!.nativeElement.innerText;
+
+    if (value !== key) return;
+
+    this.isPressed.set(true);
+
+    setTimeout(() => {
+      this.isPressed.set(false);
+    }, 100);
+  }
+}
--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  HostListener,
+  inject,
+  viewChildren,
+} from '@angular/core';
 import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+import { CalculatorService } from '@/calculator/services/calculator.service';
+import { computeMsgId } from '@angular/compiler';
 
 @Component({
   selector: 'calculator',
@@ -10,155 +18,130 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
       class="w-full h-40 bg-gradient-to-b from-gray-800 to-gray-700 flex items-end text-right"
       style="min-width: 300px;"
     >
-      <div class="w-full py-5 px-6 text-6xl text-white font-thin">340.0</div>
+      <div class="w-full py-5 px-6 text-6xl text-white font-thin">
+        @if(subResultText()!=='0') {
+        <span class="text-4xl">
+          {{ subResultText() }} {{ lastOperator() }}
+        </span>
+        <br />
+        }
+        {{ resultText() }}
+      </div>
     </div>
     <div class="w-full bg-gradient-to-b from-indigo-400 to-indigo-500">
       <div class="flex w-full">
-        <!-- <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-opacity-50 text-xl font-light"
-          >
-            C
-          </button>
-        </div> -->
-        <calculator-button> C </calculator-button>
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-opacity-50 text-xl font-light"
-          >
-            +/-
-          </button>
-        </div>
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-opacity-50 text-xl font-light"
-          >
-            %
-          </button>
-        </div>
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-2xl font-light"
-          >
-            ÷
-          </button>
-        </div>
+        <calculator-button
+          (onClick)="handleClick($event)"
+          (onClick)="handleClick($event)"
+        >
+          C
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)">
+          +/-C
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)">
+          %
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)" isCommand>
+          ÷
+        </calculator-button>
       </div>
       <div class="flex w-full">
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            7
-          </button>
-        </div>
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            8
-          </button>
-        </div>
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            9
-          </button>
-        </div>
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            ⨉
-          </button>
-        </div>
+        <calculator-button (onClick)="handleClick($event)">
+          7
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)">
+          8
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)">
+          9
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)" isCommand>
+          x
+        </calculator-button>
       </div>
       <div class="flex w-full">
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            4
-          </button>
-        </div>
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            5
-          </button>
-        </div>
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            6
-          </button>
-        </div>
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            -
-          </button>
-        </div>
+        <calculator-button (onClick)="handleClick($event)">
+          4
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)">
+          5
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)">
+          6
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)" isCommand>
+          -
+        </calculator-button>
       </div>
       <div class="flex w-full">
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            1
-          </button>
-        </div>
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            2
-          </button>
-        </div>
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            3
-          </button>
-        </div>
-        <div class="w-1/4 border-r border-b border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            +
-          </button>
-        </div>
+        <calculator-button (onClick)="handleClick($event)">
+          1
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)">
+          2
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)">
+          3
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)" isCommand>
+          +
+        </calculator-button>
       </div>
       <div class="flex w-full">
-        <div class="w-1/4 border-r border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            0
-          </button>
-        </div>
-        <div class="w-1/4 border-r border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-          >
-            .
-          </button>
-        </div>
-        <div class="w-2/4 border-r border-indigo-400">
-          <button
-            class="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-30 hover:bg-opacity-40 text-white text-xl font-light"
-          >
-            =
-          </button>
-        </div>
+        <calculator-button (onClick)="handleClick($event)">
+          0
+        </calculator-button>
+        <calculator-button (onClick)="handleClick($event)">
+          .
+        </calculator-button>
+        <calculator-button
+          (onClick)="handleClick($event)"
+          isCommand
+          isDoubleSize
+        >
+          =
+        </calculator-button>
       </div>
     </div>
   `,
-  styles: ``,
+  styles: `
+  `,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)',
+  },
 })
-export class CalculatorComponent {}
+export class CalculatorComponent {
+  private calculatorService = inject(CalculatorService);
+  public calculatorButton = viewChildren(CalculatorButtonComponent);
+
+  public resultText = computed(() => this.calculatorService.resultText());
+  public subResultText = computed(() => this.calculatorService.subResultText());
+  public lastOperator = computed(() => this.calculatorService.lastOperator());
+
+  handleClick(key: string) {
+    this.calculatorService.constructNumber(key);
+    console.log(key);
+  }
+
+  // @HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const keyEquivalents: Record<string, string> = {
+      Escape: 'C',
+      Clear: 'C',
+      x: '*',
+      X: '*',
+      '*': 'x',
+      '/': '÷',
+      Enter: '=',
+    };
+
+    const key = event.key;
+    const keyValue = keyEquivalents[key] ?? key;
+    this.handleClick(event.key);
+
+    this.calculatorButton().forEach((button) => {
+      button.keyboardPressedStyle(keyValue);
+    });
+  }
+}
